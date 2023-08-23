c79feef18feff2d2de4ede3be359f439b9033d01
Sadeep Mihiranga <sandeep@hatchyard.io>
2023-08-23 08:40:39 +0530

    Lassana

--- src/main/java/lk/hatchyard/selenium_scraper/LassanaScraper.java
+++ src/main/java/lk/hatchyard/selenium_scraper/LassanaScraper.java
@@ -35,11 +35,38 @@ public class LassanaScraper {
             System.out.println("Price : " + price);
 
             Elements link = searchedProduct.select("a[href]");
-            final String productLink =  link.attr("href");
-            System.out.println("Product link : https://lassana.com" + productLink);
+            final String productLink =  "https://lassana.com" + link.attr("href");
+            System.out.println("Product link : " + productLink);
 
 
             Document productDocument = request(productLink);
+
+            System.out.println(productDocument.selectXpath("/html/body/div[4]/div[1]/div[1]/div/div[2]/div/div[3]/div[1]/span/p[2]/a"));
+            System.out.println(productDocument.selectXpath("/html/body/div[4]/div[1]/div[1]/div/div[2]/div/div[3]/div[1]/span/p[3]/a"));
+
+            System.out.println(productDocument.select("div.product_name").select("h1.p_dark").text());
+            System.out.println(productDocument.select("div.product_name").select("p").text());
+
+            //System.out.println(productDocument.select("div.ratings"));
+            System.out.println(productDocument.select("div.reviews").select("span").select("p").first().text());
+
+            System.out.println(productDocument.select("p.orig_price").text());
+
+            System.out.println(productDocument.select("div.description").first().text());
+
+            Elements reviews = productDocument.select("div.review_details_block");
+
+            System.out.println("");
+
+            for(Element review : reviews) {
+                //System.out.println(review.text());
+                System.out.println(review.select("p.user_name_block").text());
+                System.out.println(review.select("p.rating_date").text());
+                System.out.println(review.select("div.rating_stars").text());
+                System.out.println(review.select("p.comment_block").text());
+
+                System.out.println("");
+            }
 
             break;
         }
