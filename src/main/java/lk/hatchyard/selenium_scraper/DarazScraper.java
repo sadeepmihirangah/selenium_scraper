7a72af144303b2160dd7b53ae70ebda89ded81b9
Sehan Anjelo <shehan.angelo@hatchyard.io>
2023-08-25 10:15:32 +0530

    Daraz

--- src/main/java/lk/hatchyard/selenium_scraper/DarazScraper.java
+++ src/main/java/lk/hatchyard/selenium_scraper/DarazScraper.java
@@ -8,6 +8,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class DarazScraper {
 
     public static void main(String[] args) {
@@ -20,7 +23,13 @@ public class DarazScraper {
 
         Elements elements = document.select("div.inner--SODwy ");
 
+        int count = 0;
+
         for(Element divElement : elements) {
+            count++;
+
+            if(count == 1)
+                continue;
 
             String productName = divElement.select("div.title--wFj93").text();
             System.out.println("Product name : " + productName);
@@ -41,13 +50,30 @@ public class DarazScraper {
 
             System.out.println(productDocument.select("div.tag-name").text());
 
-            Elements heroImageElement = productDocument.select("div.gallery-preview-panel__content").select("img[src]");
-            System.out.println("https:" + heroImageElement.attr("src"));
+            Elements productDetails = productDocument.select("li.key-li");
+            for(Element element : productDetails){
+                System.out.println(element.select("div.html-content.key-value").text());
+            }
+
+
+            Elements heroImageElement = productDocument.select("div.next-slick-slide.next-slick-active.item-gallery__thumbnail").select("img[src]");
+            Set<String> uniqeImages = new HashSet<>();
+            for(Element imageElement : heroImageElement) {
+                uniqeImages.add("https:" + imageElement.attr("src"));
+            }
+            uniqeImages.forEach(System.out::println);
 
 
             System.out.println(productDocument.select("div.pdp-review-summary").text());
 
             System.out.println(productDocument.select("div.html-content.detail-content").text());
+
+            Elements reviews = productDocument.select("div#module_product_review.pdp-block.module").select("div.lazyload-wrapper ");
+            Set<String> comments = new HashSet<>();
+            for(Element commentElement : reviews) {
+                comments.add("comment :" + commentElement.select("div.content").text());
+            }
+            comments.forEach(System.out::println);
 
             break;
         }
